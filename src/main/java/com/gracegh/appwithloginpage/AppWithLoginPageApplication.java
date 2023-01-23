5f3421fa94c13d74cc5be4f17743d009c3bdea97
GraceG000 <graceghansah2000@gmail.com>
2023-01-23 08:50:17 +0000

    First Push

---
+++ src/main/java/com/gracegh/appwithloginpage/AppWithLoginPageApplication.java
@@ -0,0 +1,13 @@
+package com.gracegh.appwithloginpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWithLoginPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWithLoginPageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gracegh/appwithloginpage/AppWithLoginPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gracegh.appwithloginpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWithLoginPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
